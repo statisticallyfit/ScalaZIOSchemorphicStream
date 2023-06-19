c78179e0b1e44ecfd71b9cddd86c8afd10777c1a
statisticallyfit <statisticallyfit@gmail.com>
2023-06-19 04:26:38 +0300

    changed to list format - doesn't work anymore, change back

--- src/main/scala/utilMain/utilSkeuoApache/FieldAndOrderConversions.scala
+++ src/main/scala/utilMain/utilSkeuoApache/FieldAndOrderConversions.scala
@@ -42,7 +42,12 @@ object FieldAndOrderConversions {
 			val fieldApache_schema: SchemaApacheAvro = fieldS.tpe
 			//val fieldApache_doc: String = fieldS.doc.getOrElse("") //("NO DOC")
 			val fieldApache_doc = null
-			val fieldApache_defaultValue = null
+			val fieldApache_defaultValue = null // NOTE: put assumption that null means it does not appear when printed in the Apache Schema
+			// Functions references:
+			// 	validateDefault (in Schema.java of org.apache)
+			// 	isValidDefault (in Schema.java of org.apache)
+			// 	isTextual() from JsonNodeType.java (jackson library)
+			
 			//: JsonNodeType = JsonNodeType.STRING // TODO temporary to make the string test pass.
 				//FieldApache.NULL_DEFAULT_VALUE
 			//val fieldApache_defaultValue: util.List[String] = fieldS.aliases.asJava // HELP: apache has no 'aliases' while skeuo does and skeuo has no 'default value' while apache does -- are they the same?
--- src/test/scala/conversionsOfSchemaADTs/avro_avro/skeuo_apache/specs/try_AvroToSkeuo_tatidatafile.scala
+++ src/test/scala/conversionsOfSchemaADTs/avro_avro/skeuo_apache/specs/try_AvroToSkeuo_tatidatafile.scala
@@ -45,44 +45,60 @@ object try_AvroToSkeuo_tatidatafile extends App {
 	
 	val filePath: String = "/development/projects/statisticallyfit/github/learningdataflow/SchaemeowMorphism/src/test/scala/testData/testDataPrivateTati/asset-schemas/sdp-asset-schemas-luftdaten/src/main/airquality/lft.aq_msm/lft.aq_msm.datasource/avro/"
 	
-	val name = "Sensor.avsc"
+	// TODO how to concat files resulting from the avro-tools' conversion of .avdl to .avsc?
+	val fileType: String = ".avsc"
+	val names: List[String] = List("Aq_Msm", /*"Location",*/ "Sensor", "Sensor_type", "Sensordatavalues_record")
+	
 	
 	//val avroFile = new java.io.File(filePath + name)
-	val avroFile: String = filePath + name
-	val avroFileObj: File = new File(avroFile)
+	val avroStrFiles: List[String] = names.map(_ + fileType)
+	val avroStrFilePaths: List[String] = names.map(n ⇒ filePath + n + fileType)
+	val avroStrFileObjs: List[File] = avroStrFilePaths.map(new File(_))
 	
 	// Convert .avsc to schema-avro
-	val parsedAvroSchemaFromFile: SchemaAvro_Apache = new SchemaAvro_Apache.Parser().parse(avroFileObj)
-	val mpprintout = Map("getDoc" → parsedAvroSchemaFromFile.getDoc,
-		"getFields" → parsedAvroSchemaFromFile.getFields,
-		"getField(\"manufacturer\")" → parsedAvroSchemaFromFile.getField("sensor_type"),
-		"field doc" → parsedAvroSchemaFromFile.getField("sensor_type").doc(),
-		"field order" → parsedAvroSchemaFromFile.getField("sensor_type").order(),
-		"field schema" → parsedAvroSchemaFromFile.getField("sensor_type").schema(),
-		"field aliases" → parsedAvroSchemaFromFile.getField("sensor_type").aliases(),
-		"field defaultval" → parsedAvroSchemaFromFile.getField("sensor_type").defaultVal(),
-		"field hasdefaultval" → parsedAvroSchemaFromFile.getField("sensor_type").hasDefaultValue,
-		"field objectprops" → parsedAvroSchemaFromFile.getField("sensor_type").getObjectProps
+	val parserApacheStrToADT: SchemaAvro_Apache.Parser = new SchemaAvro_Apache.Parser()
+	
+	
+	//new SchemaAvro_Apache(SchemaAvro_Apache.Type.RECORD)
+	// TODO how to check that Names contains key?
+	
+	val parsedApaches: Map[String, SchemaAvro_Apache] = avroStrFileObjs.map{fileObj ⇒ (fileObj.getName → parserApacheStrToADT.parse(fileObj)) }.toMap
+	
+	// TODO why doesn't see it as Map already (beacuse of usage of -> ??) why have to declare .toMap explicitly?
+	
+	/*val parsedSensor: Option[SchemaAvro_Apache] = parsedApaches.get("Sensor" + fileType)
+	val tempMapPrintoutSensor = Map("getDoc" → parsedSensor.getDoc,
+		"getFields" → parsedSensor.getFields,
+		"getField(\"manufacturer\")" → parsedSensor.getField("sensor_type"),
+		"field doc" → parsedSensor.getField("sensor_type").doc(),
+		"field order" → parsedSensor.getField("sensor_type").order(),
+		"field schema" → parsedSensor.getField("sensor_type").schema(),
+		"field aliases" → parsedSensor.getField("sensor_type").aliases(),
+		"field defaultval" → parsedSensor.getField("sensor_type").defaultVal(),
+		"field hasdefaultval" → parsedSensor.getField("sensor_type").hasDefaultValue,
+		"field objectprops" → parsedSensor.getField("sensor_type").getObjectProps
 	)
-	println(mpprintout)
+	println(tempMapPrintoutSensor)*/
 	
 	// Convert apache schema -> skeuo schema
-	val skeuoSchema: Fix[SchemaAvro_Skeuo] = apacheToSkeuoAvroSchema(parsedAvroSchemaFromFile)
+	val skeuoSchemas: Map[String, Fix[SchemaAvro_Skeuo]] = parsedApaches.map{ case (name, parsedApache) ⇒ (name → apacheToSkeuoAvroSchema(parsedApache)) }
 	
 	// Converting backward
-	val avroBack: SchemaAvro_Apache = skeuoToApacheAvroSchema(skeuoSchema)
+	val avrosBack: Map[String, SchemaAvro_Apache] = skeuoSchemas.map{ case (name, apacheSchema) ⇒ (name → skeuoToApacheAvroSchema(apacheSchema)) }
 	
 	// Get reference to source object so can close again when done reading the file contents
-	val srcObj: BufferedSource = Source.fromFile(avroFile)
-	val avroStr: String = srcObj.getLines().mkString
-	srcObj.close // closing the file
+	val bufferedSourceList: List[BufferedSource] = avroStrFilePaths.map((path: String) ⇒ Source.fromFile(path))
+	val avroStrList: List[String] = bufferedSourceList.map(buf ⇒ buf.getLines().mkString)
+	// Closing each buffered source
+	bufferedSourceList.map(buf ⇒ buf.close)
 	
-	println(s"original avro string = ${avroStr}")
-	println(s"converted avro string to APACHE SCHEMA = ${parsedAvroSchemaFromFile}")
-	println(s"apache avro schema to SKEUO schema = ${skeuoSchema}")
-	println(s"skeuo -> apache back: $avroBack")
 	
-	println(s"apache schema == apache back: ${parsedAvroSchemaFromFile == avroBack}")
-	println(s" (str) apache schema == apache back: ${parsedAvroSchemaFromFile.toString == avroBack.toString}")
+	println(s"ORIGINAL CONTENTS OF .AVSC FILE (FROM .AVDL): ${avroStrList}")
+	println(s"\n\nCONVERTED: ORIGINAL .AVSC ---> APACHE SCHEMA: ${parsedApaches}")
+	println(s"\n\nAPACHE ---> SKEUO: ${skeuoSchemas}")
+	println(s"\n\nSKEUO ---> APACHE back: $avrosBack")
+	
+	println(s"apache schema == apache back: ${parsedApaches == avrosBack}")
+	//println(s" (str) apache schema == apache back: ${parsedApaches.toString == avrosBack.toString}")
 	
 }
